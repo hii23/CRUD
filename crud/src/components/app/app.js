19a2e210258c49156802c1900f62ac9259f69df3
hii23 <flrserhii@gmail.com>
2021-10-19 00:04:59 +0300

    add props and algorithm reconciliation

--- crud/src/components/app/app.js
+++ crud/src/components/app/app.js
@@ -8,6 +8,12 @@ import EmployeesAddForm from "../employees-add-form/employees-add-form";
 
 
 function App() {
+
+    const data = [
+        {name: 'John S.',salary: 800,increase:false,id:1},
+        {name: 'Alex B.',salary: 1200,increase:true,id:2},
+        {name: 'Ted L.',salary: 500,increase:false,id:3},
+    ]
     return (
         <div className={'app'}>
             <AppInfo/>
@@ -16,7 +22,7 @@ function App() {
                 <SearchPanel/>
                 <AppFilter/>
             </div>
-            <EmployeesList/>
+            <EmployeesList data={data}/>
             <EmployeesAddForm/>
         </div>
     )
