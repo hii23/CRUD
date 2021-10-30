70ae21ca3897363a2586b2568ce9fafe93fa33b1
hii23 <flrserhii@gmail.com>
2021-10-30 23:21:17 +0300

    add filter btns

--- crud/src/components/app-filter/app-filter.js
+++ crud/src/components/app-filter/app-filter.js
@@ -1,23 +1,26 @@
 import './app-filter.css'
 
-const AppFilter = () => {
+const AppFilter = ({filter, onUpdateFilter}) => {
+    const buttonsDate = [
+        {name: 'all', label: 'Все сотрудники'},
+        {name: 'rise', label: 'На повышение'},
+        {name: 'salary', label: 'З/П больше 1000$'},
+    ];
+    const buttons = buttonsDate.map(({name, label}) => {
+        return (
+            <button
+                className={`btn  ${filter === name && `btn-light` || 'btn-outline-light'}`}
+                type={'button'}
+                key={name}
+                onClick={() => onUpdateFilter(name)}
+            >
+                {label}
+            </button>
+        )
+    })
     return (
         <div className={'btn-group'}>
-            <button
-                className={'btn btn-light'}
-                type={'button'}>
-                    Все сотрудники
-            </button>
-            <button
-                className={'btn btn-outline-light'}
-                type={'button'}>
-                    На повышение
-            </button>
-            <button
-                className={'btn btn-outline-light'}
-                type={'button'}>
-                    З/П больше 1000$
-            </button>
+            {buttons}
         </div>
     )
 }
--- crud/src/components/app/app.js
+++ crud/src/components/app/app.js
@@ -20,6 +20,7 @@ class App extends Component {
             ],
             idMax: 4,
             term: '',
+            filter: 'app',
         }
     }
 
@@ -42,16 +43,6 @@ class App extends Component {
         }))
     }
 
-    searchItem = (items, term) => {
-        return items.filter(item => {
-            return item.name.indexOf(term) > -1
-        })
-    }
-
-    onUpdateSearch = (term) => {
-        this.setState(({term}))
-    }
-
     addItem = (event, name, salary) => {
         if (name && salary) {
             event.preventDefault();
@@ -67,12 +58,38 @@ class App extends Component {
 
     }
 
+    searchItem = (items, term) => {
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState(({term}))
+    }
+
+    filterPost = (items, filter) => {
+        switch (filter) {
+            case "rise":
+                return items.filter(item => item.rise)
+            case "salary":
+                return items.filter(item => item.salary > 1000)
+            default:
+                return items
+        }
+    }
+
+    onUpdateFilter = (filter) => {
+        this.setState(({filter}))
+    }
+
+
     render() {
 
-        const {data, term} = this.state;
+        const {data, term, filter} = this.state;
         const employees = data.length;
         const increased = data.filter(item => item.increase).length;
-        const visibleDate = this.searchItem(data, term)
+        const visibleDate = this.filterPost(this.searchItem(data, term), filter);
         return (
             <div className={'app'}>
                 <AppInfo
@@ -84,7 +101,10 @@ class App extends Component {
                     <SearchPanel
                         onUpdateSearch={this.onUpdateSearch}
                     />
-                    <AppFilter/>
+                    <AppFilter
+                        filter={filter}
+                        onUpdateFilter={this.onUpdateFilter}
+                    />
                 </div>
                 <EmployeesList
                     data={visibleDate}
